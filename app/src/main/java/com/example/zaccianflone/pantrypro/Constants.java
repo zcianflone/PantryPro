326835099257434c4b3e474aa9afd1968ff3ebbc
Zac Cianflone <zcianflone@gmail.com>
2016-06-18 21:55:21 -0400

    Zac Test Cases

---
+++ app/src/main/java/com/example/zaccianflone/pantrypro/Constants.java
@@ -0,0 +1,13 @@
+package com.example.zaccianflone.pantrypro;
+
+/**
+ * Created by zaccianflone on 6/18/16.
+ */
+public final class Constants {
+
+    /**
+     * Constants for Firebase URL
+     */
+
+    public static final String FIREBASE_URL="https://fiery-inferno-4832.firebaseio.com";
+}
