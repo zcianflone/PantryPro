7fc92e5bf9824f2b0d14986ed81c2641a2212fe2
bwright400 <bwright400@gmail.com>
2016-07-02 01:49:31 -0600

    making a connector class

--- app/src/main/java/com/example/zaccianflone/pantrypro/AddItemRecipe.java
+++ app/src/main/java/com/example/zaccianflone/pantrypro/AddItemRecipe.java
@@ -1,24 +1,39 @@
 package com.example.zaccianflone.pantrypro;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class AddItemRecipe extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item_recipe);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
      * When the user clicks back go to Main Activity
+     *
      * @param view
      */
     public void goBack(View view) {
         Intent intent = new Intent(this, MakeRecipe.class);
         startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/example/zaccianflone/pantrypro/database/Connector.java
@@ -0,0 +1,8 @@
+package com.example.zaccianflone.pantrypro.database;
+
+/**
+ * Created by Ben on 7/2/2016.
+ */
+public class Connector {
+
+}
