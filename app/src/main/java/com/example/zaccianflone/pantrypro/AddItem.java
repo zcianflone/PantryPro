bf60d1c756e0ed29fa60103083130c6671824bf3
Zac Cianflone <zcianflone@gmail.com>
2016-07-01 22:54:47 -0400

    Fixed Issue where blank items could be added to FireBase

--- app/src/main/java/com/example/zaccianflone/pantrypro/AddItem.java
+++ app/src/main/java/com/example/zaccianflone/pantrypro/AddItem.java
@@ -44,25 +44,54 @@ public class AddItem extends AppCompatActivity {
      */
     public void addItem (View view) {
 
+        boolean addToast = false;
+
         EditText name = (EditText) findViewById(R.id.editText6);
         EditText expdate = (EditText) findViewById(R.id.editText7);
         EditText quantity = (EditText) findViewById(R.id.editText8);
         EditText unit = (EditText) findViewById(R.id.editText9);
 
         Firebase ref=new Firebase ("https://fiery-inferno-4832.firebaseio.com");
-        ref.child("pantry").child(name.getText().toString()).child("name").setValue(name.getText().toString());
-        ref.child("pantry").child(name.getText().toString()).child("expdate").setValue(expdate.getText().toString());
-        ref.child("pantry").child(name.getText().toString()).child("quantity").setValue(quantity.getText().toString());
-        ref.child("pantry").child(name.getText().toString()).child("unit").setValue(unit.getText().toString());
+
+        /*
+        This form validation needs to get more robust with better regex validation.  As it stands now,
+        this will only write to the firebase if the field isn't empty.
+
+        If the name field is empty, we don't bother testing anything else because we wouldn't want a
+        nameless "item"
+         */
+        if (!name.getText().toString().matches("")){
+            ref.child("pantry").child(name.getText().toString()).child("name").setValue(name.getText().toString());
+            addToast = true;
+
+            if (!expdate.getText().toString().matches("")){
+                ref.child("pantry").child(name.getText().toString()).child("expdate").setValue(expdate.getText().toString());
+            }
+
+            if (!quantity.getText().toString().matches("")){
+                ref.child("pantry").child(name.getText().toString()).child("quantity").setValue(quantity.getText().toString());
+            }
+            else //if the user doesn't specify a quantity, we default a quantity of 1
+            {
+                ref.child("pantry").child(name.getText().toString()).child("quantity").setValue("1");
+            }
+
+            if (!unit.getText().toString().matches("")){
+                ref.child("pantry").child(name.getText().toString()).child("unit").setValue(unit.getText().toString());
+            }
+        }
+
 
         Log.i("Firebase New Item Loc", "pantry>"+name.getText().toString()+">"+"name>"+name.getText().toString());
 
         // Display toast message at bottom of screen when an item has been added
-        Context context = getApplicationContext();
-        CharSequence text = "Item added to pantry";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        if (addToast) {
+            Context context = getApplicationContext();
+            CharSequence text = "Item added to pantry";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 
 }
