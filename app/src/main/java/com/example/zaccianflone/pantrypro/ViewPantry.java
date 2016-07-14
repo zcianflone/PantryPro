4a96183f13c3fce0c6eb84f11665be5744c841bf
Zac Cianflone <zcianflone@gmail.com>
2016-07-14 03:37:55 -0400

    Built Spinner into ViewPantry that Displays sorted ListViews

--- app/src/main/java/com/example/zaccianflone/pantrypro/ViewPantry.java
+++ app/src/main/java/com/example/zaccianflone/pantrypro/ViewPantry.java
@@ -3,23 +3,30 @@ package com.example.zaccianflone.pantrypro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zaccianflone.pantrypro.model.PantryItem;
 import com.firebase.client.Firebase;
 import com.firebase.ui.FirebaseListAdapter;
 
-public class ViewPantry extends AppCompatActivity {
+public class ViewPantry extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+
+    Firebase ref = new Firebase("https://pantrypro-a7109.firebaseio.com/pantry");
+
+    FirebaseListAdapter<PantryItem> fbAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pantry);
+        ListView mListView = (ListView) findViewById(R.id.listView);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array located in strings.xml
@@ -27,23 +34,15 @@ public class ViewPantry extends AppCompatActivity {
                 R.array.viewPantrySpinner, android.R.layout.simple_spinner_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
-        ListView mListView = (ListView) findViewById(R.id.listView);
-
-        Firebase ref = new Firebase("https://pantrypro-a7109.firebaseio.com/pantry");
-
-        final FirebaseListAdapter<PantryItem> fbAdapter =
-                new FirebaseListAdapter<PantryItem>(this, PantryItem.class, android.R.layout.simple_list_item_1, ref) {
-                    @Override
-                    protected void populateView(View view, PantryItem pantryItem, int position) {
-                        TextView textView = (TextView)view.findViewById(android.R.id.text1);
+        spinner.setOnItemSelectedListener(this);
 
 
-                        textView.setText(pantryItem.getName());
-                    }
-                };
+
         mListView.setAdapter(fbAdapter);
 
+        /**
+         * This passes the pushID of the clicked item to the PantryListDetails Activity
+         */
        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -62,7 +61,11 @@ public class ViewPantry extends AppCompatActivity {
             }
         });
 
+
+
     }
+
+
 
     /**
      * When the user clicks back go to Main Activity
@@ -72,5 +75,83 @@ public class ViewPantry extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        ListView mListView = (ListView) findViewById(R.id.listView);
+        TextView myText = (TextView) view;
+
+        Log.d("in item selected", myText.getText().toString() );
+
+        if (position == 1) {
+            fbAdapter =
+                    new FirebaseListAdapter<PantryItem>(this, PantryItem.class, android.R.layout.simple_list_item_1, ref) {
+                        @Override
+                        protected void populateView(View view, PantryItem pantryItem, int position) {
+                            TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+
+                            textView.setText(pantryItem.getName());
+                        }
+                    };
+
+        }
+        else if (position == 2)
+        {
+            fbAdapter =
+                    new FirebaseListAdapter<PantryItem>(this, PantryItem.class, android.R.layout.simple_list_item_1, ref.orderByChild("name")) {
+                        @Override
+                        protected void populateView(View view, PantryItem pantryItem, int position) {
+                            TextView textView = (TextView)view.findViewById(android.R.id.text1);
+
+
+                            textView.setText(pantryItem.getName());
+                        }
+                    };
+        }
+
+        else if (position == 3)
+        {
+            fbAdapter =
+                    new FirebaseListAdapter<PantryItem>(this, PantryItem.class, android.R.layout.simple_list_item_1, ref.orderByChild("expDate")) {
+                        @Override
+                        protected void populateView(View view, PantryItem pantryItem, int position) {
+                            TextView textView = (TextView)view.findViewById(android.R.id.text1);
+
+
+                            textView.setText(pantryItem.getName() +"    Exp: "+ pantryItem.getExpDate());
+                        }
+                    };
+        }
+
+        else{
+            fbAdapter =
+                    new FirebaseListAdapter<PantryItem>(this, PantryItem.class, android.R.layout.simple_list_item_1, ref.orderByChild("invertedTime")) {
+                        @Override
+                        protected void populateView(View view, PantryItem pantryItem, int position) {
+                            TextView textView = (TextView)view.findViewById(android.R.id.text1);
+
+
+                            textView.setText(pantryItem.getName());
+                        }
+                    };
+        }
+
+            Toast.makeText(this, "You Selected " +myText.getText(), Toast.LENGTH_SHORT).show();
+
+            fbAdapter.notifyDataSetChanged();
+            mListView.setAdapter(fbAdapter);
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }
 
--- app/src/main/java/com/example/zaccianflone/pantrypro/model/PantryItem.java
+++ app/src/main/java/com/example/zaccianflone/pantrypro/model/PantryItem.java
@@ -1,5 +1,11 @@
 package com.example.zaccianflone.pantrypro.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.firebase.client.ServerValue;
+
+import java.util.HashMap;
+import java.util.Date;
+
 /**
  * Created by zaccianflone on 7/7/16.
  */
@@ -9,6 +15,8 @@ public class PantryItem {
     String expDate;
     String quantity;
     String unitType;
+    long invertedTime;
+    private HashMap<String, Object> timestampLastChanged;
 
     public PantryItem() {
     }
@@ -18,6 +26,14 @@ public class PantryItem {
         this.expDate = expDate;
         this.quantity = quantity;
         this.unitType = unitType;
+
+
+        HashMap<String, Object> timestampLastChangedObj = new HashMap<String, Object>();
+        timestampLastChangedObj.put("timestamp", ServerValue.TIMESTAMP);
+        this.timestampLastChanged = timestampLastChangedObj;
+
+        long d = (long) new Date().getTime();
+        this.invertedTime = 0-d;
     }
 
     public String getExpDate() {
@@ -35,4 +51,17 @@ public class PantryItem {
     public String getUnitType() {
         return unitType;
     }
+
+    public HashMap<String, Object> getTimestampLastChanged() {
+        return timestampLastChanged;
+    }
+
+    public long getInvertedTime(){return invertedTime;}
+
+    @JsonIgnore
+    public long getTimestampLastChangedLong() {
+        return (long) timestampLastChanged.get("timestamp");
+    }
+
+
 }
