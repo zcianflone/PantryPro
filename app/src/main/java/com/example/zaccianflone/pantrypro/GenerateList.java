b35bafb33a968f799e899965b00ecb20cd9908a0
Zac Cianflone <zcianflone@gmail.com>
2016-07-17 04:29:12 -0400

    Generate Shopping List Complete

--- app/src/main/java/com/example/zaccianflone/pantrypro/GenerateList.java
+++ app/src/main/java/com/example/zaccianflone/pantrypro/GenerateList.java
@@ -3,7 +3,6 @@ package com.example.zaccianflone.pantrypro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,6 +25,8 @@ public class GenerateList extends AppCompatActivity {
     Firebase ingredientRef, pantryRef;
     String recipeName;
     ArrayList<String> recipeIngredients = new ArrayList<String>();
+    ArrayList<String> pantryIngredients = new ArrayList<String>();
+    ArrayList<String> neededIngredients = new ArrayList<String>();
     ArrayAdapter<String> arrayAdapter;
 
 
@@ -50,16 +51,16 @@ public class GenerateList extends AppCompatActivity {
                     String name = (String) child.child("recipeName").getValue();
                     String item = (String) child.child("name").getValue();
 
-                    Log.d("ingred name", name);
-                    Log.d("recipe name", recipeName);
+                   // Log.d("ingred name", name);
+                    //Log.d("recipe name", recipeName);
 
                     if (name.equals(recipeName)){
                         recipeIngredients.add(item);
 
-                        Log.d("inside g", recipeName);
+                        //Log.d("inside g", recipeName);
 
                         for (String items : recipeIngredients){
-                            Log.d("items 1", items);
+                           // Log.d("items 1", items);
                         }
 
 
@@ -76,12 +77,49 @@ public class GenerateList extends AppCompatActivity {
             }
         });
 
+        pantryRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot child : snapshot.getChildren()) {
+
+                    String item = (String) child.child("name").getValue();
+
+                    pantryIngredients.add(item);
+
+                }
+
+                for (String items : recipeIngredients) {
+                    if (!pantryIngredients.contains(items)) {
+
+                        neededIngredients.add(items);
+                    }
+                }
+
+
+
+
+
+
+            }
+
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                finish();
+                return;
+            }
+        });
+
+
+
+
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                recipeIngredients);
+                neededIngredients);
 
         mListView.setAdapter(arrayAdapter);
 
-       
+
     }
 
 
